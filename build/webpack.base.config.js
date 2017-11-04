78beba9f01b7773d51b1e348fe5ad74fa41a40f8
luzuoquan <luzuoquanflex@gmail.com>
2017-11-04 11:29:10 +0800

    update

--- build/webpack.base.config.js
+++ build/webpack.base.config.js
@@ -21,8 +21,7 @@ module.exports = {
       exclude: /node_modules/
     }, {
       test: /\.css/,
-      use: ['style-loader','css-loader'],
-      exclude: /node_modules/
+      use: ['style-loader','css-loader']
     }]
   }
 }
--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-import Cropper from 'cropperjs'
-import './src/assets/css/cropper.min.css'
+import Swiper from 'swiper'
+import 'normalize.css'
+import './src/assets/css/main.css'
 
-const image = document.querySelector('#img')
-
-const cropper = new Cropper(image, {
-  aspectRatio: 16 / 9,
-  crop: function(e) {
-    console.log(e.detail)
-  }
+new Swiper('.swiper-container', {
+  direction: 'vertical'
 })
+
+
