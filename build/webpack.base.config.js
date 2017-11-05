3d860e29e6e5c99eacfb743b0a4cf0b61fbedfbd
luzuoquan <luzuoquanflex@gmail.com>
2017-11-05 11:18:22 +0800

    update

--- build/webpack.base.config.js
+++ build/webpack.base.config.js
@@ -17,11 +17,20 @@ module.exports = {
   module: {
     rules: [{
       test: /\.(js|jsx)$/,
-      loader: 'babel-loader',
+      use: 'babel-loader',
       exclude: /node_modules/
     }, {
       test: /\.css/,
       use: ['style-loader','css-loader']
+    }, {
+      test: require.resolve("../src/assets/lib/jquery-3.2.1.min.js"),
+      use: "imports-loader?$=jquery"
     }]
-  }
+  },
+  plugins:[
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery'
+    })
+  ]
 }
--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 import Swiper from 'swiper'
+import 'typeit'
 import 'normalize.css'
 import './src/assets/css/main.css'
 
+console.info($)
+
 new Swiper('.swiper-container', {
   direction: 'vertical'
+})
+
+$('.type-it').typeIt({
+  speed: 200,
+  cursor: false
 })
 
 
