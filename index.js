9be8a8670913272eb541cfbac7cf3541c0200a3e
luzuoquan <luzuoquanflex@gmail.com>
2017-11-11 18:48:00 +0800

    update

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import Swiper from 'swiper'
 import './src/assets/lib/createjs.js'
 import 'normalize.css'
 import './src/assets/css/main.css'
+import { setTimeout } from 'timers';
 
 // require('html-loader!./index.html')
 
@@ -10,11 +11,19 @@ const queue = new createjs.LoadQueue(true)
 const manifest = [
   {id: '#J-slide-1', src: require('./src/assets/images/cover.png')},
   {id: '#J-slide-2', src: require('./src/assets/images/bg.png')},
-  {id: '#J-slide-3', src: require('./src/assets/images/data-1.png')},
-  {id: '#J-slide-4', src: require('./src/assets/images/step-1.png')},
-  {id: '#J-slide-5', src: require('./src/assets/images/step-2.png')},
-  {id: '#J-slide-6', src: require('./src/assets/images/step-3.png')},
-  {id: '#J-slide-7', src: require('./src/assets/images/step-4.png')}
+  {id: '#J-slide-3', src: require('./src/assets/images/bg.png')},
+  {id: '#J-slide-4', src: require('./src/assets/images/bg.png')},
+  {id: '#J-slide-5', src: require('./src/assets/images/bg.png')},
+  {id: '#J-slide-6', src: require('./src/assets/images/step-1.png')},
+  {id: '#J-slide-7', src: require('./src/assets/images/step-2-1.png')},
+  {id: '#J-slide-7', src: require('./src/assets/images/step-2.png')},
+]
+
+const frontManifest = [
+  {id: '#J-slide-front-2', src: require('./src/assets/images/front-1.png')},
+  {id: '#J-slide-front-3', src: require('./src/assets/images/front-2.png')},
+  {id: '#J-slide-front-4', src: require('./src/assets/images/front-3.png')},
+  {id: '#J-slide-front-5', src: require('./src/assets/images/front-4.png')}
 ]
 
 
@@ -28,18 +37,40 @@ const handleComplete = () => {
   manifest.forEach(item => {
     $(item.id).css('background-image', `url(${item.src}`)
   })
+  frontManifest.forEach(item => {
+    $(item.id).css('background-image', `url(${item.src}`)
+  })
 }
 
 queue.on("progress", handleProgress, this)
 
 queue.on("complete", handleComplete, this)
 
-
-
 const swiper = new Swiper('.swiper-container', {
   direction: 'vertical',
   simulateTouch: false,
-  followFinger: false
+  followFinger: false,
+  on: {
+    slideChangeTransitionStart: function () {
+      console.info(this)
+      this.allowSlideNext = this.activeIndex !== 6
+    }
+  }
+})
+
+$('.u-role').on('click', function () {
+  if ($(this).attr('data-type') !== 'newer') {
+    $('#J-slide-7').css('background-image', `url(${require('./src/assets/images/step-2.png')}`)
+    $('#J-register-btn').hide();
+  } else {
+    $('#J-register-btn').show();
+    $('#J-slide-7').css('background-image', `url(${require('./src/assets/images/step-2-1.png')}`)
+  }
+  swiper.allowSlideNext = true
+  swiper.slideNext(300, false)
+  setTimeout(() => {
+    swiper.allowSlideNext = false
+  },0)
 })
 
 
