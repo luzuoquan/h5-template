710ec7c5342f349f1fd1ae3796b8fa3a4b9209ae
hzluzuoquan <hzluzuoquan@corp.netease.com>
2017-11-16 12:14:58 +0800

    update

--- src/js/draw.js
+++ src/js/draw.js
@@ -14,15 +14,17 @@ export default class Draw {
   draw () {
     const devicePixelRatio = window.devicePixelRatio || 1
     const image = new Image()
+    const textTop = this.height / 2
     image.setAttribute('crossOrigin', 'anonymous')
     image.src = this.src
     this.canvas.width = this.width * devicePixelRatio
     this.canvas.height = this.height * devicePixelRatio
     this.canvasContext.scale(devicePixelRatio, devicePixelRatio)
+    
     image.onload = () => {
       this.canvasContext.clearRect(0, 0, 1000, 1000)
       this.canvasContext.drawImage(image, 0, 0, this.width, this.height)
-      this.canvasContext.font = 'italic bold 24px PingFangSC-Light,Helvetica'
+      this.canvasContext.font = 'italic normal 24px PingFangSC-Light,Helvetica'
       this.canvasContext.fillStyle = 'white'
       this.canvasContext.textAlign = 'center'
       const splitIndex = this.title.lastIndexOf(this.type === 1 ? '加' : '第')
@@ -32,12 +34,12 @@ export default class Draw {
       this.canvasContext.fillText(this.title.substr(splitIndex), this.width / 2, 184)
 
       // description
-      this.canvasContext.font = 'normal bold 22px PingFangSC-Light,Helvetica'
+      this.canvasContext.font = 'normal normal 20px PingFangSC-Light,Helvetica'
       if (this.description.length > 10) {
-        this.canvasContext.fillText(this.description.substr(0, 10), this.width / 2, 250)
-        this.canvasContext.fillText(this.description.substr(10), this.width / 2, 284)
+        this.canvasContext.fillText(this.description.substr(0, 10), this.width / 2, textTop - 20)
+        this.canvasContext.fillText(this.description.substr(10), this.width / 2, textTop + 10)
       } else {
-        this.canvasContext.fillText(this.description, this.width / 2, 250)
+        this.canvasContext.fillText(this.description, this.width / 2, textTop -20)
       }
     }
   }
