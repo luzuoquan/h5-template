0fa6779781b5feaa5323f48833e3d9e49c7b8228
luzuoquan <luzuoquanflex@gmail.com>
2017-12-09 22:51:41 +0800

    update

--- index.js
+++ index.js
@@ -2,3 +2,7 @@
 // import 'normalize.css'
 import './src/assets/css/home.css'
 import Parallax from 'parallax-js'
+
+const scene1 = document.querySelector('#J-scene')
+
+const parallaxInstance = new Parallax(scene1)
