be812713f6af9e8bb32a81ada67ea3f8709e2488
luzuoquan <luzuoquanflex@gmail.com>
2017-12-11 00:21:11 +0800

    update

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // @flow
 import './src/assets/css/home.css'
-import Swiper from 'swiper'
+// import Swiper from 'swiper'
+import { throttle } from 'lodash'
+import 'zepto/src/touch'
 // import $ from 'jquery'
 
 // import Parallax from 'parallax-js'
@@ -9,13 +11,38 @@ import Swiper from 'swiper'
 
 // const parallaxInstance = new Parallax(scene1)
 
-console.info($('body'))
+// const swiper = new Swiper('.swiper-container', {
+//   direction: 'vertical',
+//   parallax: true,
+//   freeMode: true,
+//   mousewheel: true
+// })
+
+const bodyHeight = $('body').height()
+let parllax1 = {}
+let startY = 0
+let endY = 0
+
+console.info(bodyHeight)
 
 $(document).on('DOMContentLoaded', function(event) {
-  
-  console.info($('#J-parallax-1').offset())
+  parllax1 = $('#J-parallax-1').offset()
 })
 
-$(window).on('scroll', function(event) {
-  console.info(window.scrollY)
+// $('body').on('touchstart', function(event) {
+//   console.info(event)
+//   alert('123')
+//   if (window.scrollY + bodyHeight - parllax1.top > 0) {
+//     $('#J-parallax-1').find('.u-parallax-item').css({
+//       'transform': 'translate(0, -100px)'
+//     })
+//   }
+// })
+
+// $('body').on('touchmove', function(event) {
+//   console.info(`move,${event}`)
+// })
+
+$('body').on('swipe', function(event) {
+  console.info(event)
 })
