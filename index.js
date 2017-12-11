49c30b8072a5700868d07385f5e52829f7c5241f
hzluzuoquan <hzluzuoquan@corp.netease.com>
2017-12-11 19:52:41 +0800

    update

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ import './src/assets/css/home.css'
 // import Swiper from 'swiper'
 import { throttle } from 'lodash'
 import 'zepto/src/touch'
-// import $ from 'jquery'
 
 // import Parallax from 'parallax-js'
 
@@ -21,28 +20,91 @@ import 'zepto/src/touch'
 const bodyHeight = $('body').height()
 let parllax1 = {}
 let startY = 0
-let endY = 0
+let startX = 0
+let transformY = 0
 
-console.info(bodyHeight)
+function getAngle (angx, angy) {  
+  return Math.atan2(angy, angx) * 180 / Math.PI 
+}
+
+function getDirection (startx, starty, endx, endy) {  
+  let angx = endx - startx
+  let angy = endy - starty
+  let result = 0
+  //如果滑动距离太短  
+  if (Math.abs(angx) < 2 && Math.abs(angy) < 2) {
+    return result
+  }
+  let angle = getAngle(angx, angy)
+  if (angle >= -135 && angle <= -45) {
+      result = 1
+  } else if (angle > 45 && angle < 135) {
+      result = 2
+  } else if ((angle >= 135 && angle <= 180) || (angle >= -180 && angle < -135)) {
+      result = 3
+  } else if (angle >= -45 && angle <= 45) {
+      result = 4
+  }
+  return result
+}
 
 $(document).on('DOMContentLoaded', function(event) {
   parllax1 = $('#J-parallax-1').offset()
+  console.info(parllax1)
 })
 
-// $('body').on('touchstart', function(event) {
-//   console.info(event)
-//   alert('123')
-//   if (window.scrollY + bodyHeight - parllax1.top > 0) {
-//     $('#J-parallax-1').find('.u-parallax-item').css({
-//       'transform': 'translate(0, -100px)'
-//     })
-//   }
-// })
+$('body').on('touchstart', function(event) {
+  startX = event.touches[0].pageX
+  startY = event.touches[0].pageY
+  console.info(startX, startY)
+  // alert('123')
+  // if (window.scrollY + bodyHeight - parllax1.top > 0) {
+  //   $('#J-parallax-1').find('.u-parallax-item').css({
+  //     'transform': 'translate(0, -100px)'
+  //   })
+  // }
+})
 
-// $('body').on('touchmove', function(event) {
-//   console.info(`move,${event}`)
-// })
+$('body').on('touchmove', function(event) {
+  let endX = 0
+  let endY = 0
+  endX = event.changedTouches[0].pageX
+  endY = event.changedTouches[0].pageY
+  const direction = getDirection(startX, startY, endX, endY)
 
-$('body').on('swipe', function(event) {
-  console.info(event)
+  transformY += startY - endY
+
+  if (direction === 1) {
+    const limit = window.scrollY + bodyHeight - parllax1.top
+    console.info('持续向上',window.scrollY, bodyHeight, limit)
+    if ((bodyHeight / 2) < limit && limit < (parllax1.height + bodyHeight / 2) ) {
+      $('#J-parallax-1').find('.u-parallax-item').css({
+        'transform': `translate(0, -${transformY}px)`
+      })
+    }
+  }
+
+  if (direction === 2) {
+    // console.info(window.scrollY)
+  }
+})
+
+$('body').on('touchend', function(event) {
+  let endX = 0
+  let endY = 0
+  endX = event.changedTouches[0].pageX
+  endY = event.changedTouches[0].pageY
+  const direction = getDirection(startX, startY, endX, endY)
+
+  transformY = 0 // 重置0
+  
+  if (direction === 1) {
+    console.info('end向上')
+  }
+
+  if (direction === 2) {
+    console.info('end向下')
+  }
+
+  // console.info(`move,${event}`)
 })
